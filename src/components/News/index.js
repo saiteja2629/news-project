6403e80939a1b3765dc3b9a42dcb8d48d5d06acd
sirisha <sirishagudipudi50@gmail.com>
2024-03-28 15:16:06 +0530

    first commit

---
+++ src/components/News/index.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const News = () => {
+  const [search, setSearch] = useState("");
+
+  const [newsData, setNewsData] = useState(null);
+
+  const fetchNewsData = async (param) => {
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/everything?q=${param}&apiKey=50d7788ad43c41acac8a48eb64158c2a`
+      );
+
+      const data = await response.json();
+
+      if (data) {
+        setNewsData(data.articles);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onChangeInput = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const changeNews = () => {
+    fetchNewsData(search);
+  };
+
+  return (
+    <div className="main-news">
+      <div className="input-search-container">
+        <h3>News App</h3>
+
+        <h1>GO INDIA </h1>
+
+        <input
+          type="text"
+          placeholder="search for news...."
+          value={search}
+          onChange={onChangeInput}
+        ></input>
+
+        <button onClick={changeNews}>Search</button>
+      </div>
+
+      <marquee className="scroll">
+        The idea of redemption is always good news, even if it means sacrifice
+        or some difficult times
+      </marquee>
+
+      <div>
+        <img
+          className="image2"
+          src="https://cdn.pixabay.com/photo/2016/02/01/00/56/news-1172463_640.jpg"
+          alt="img"
+        ></img>
+      </div>
+
+      <ul>
+        {newsData?.map((article) => {
+          return (
+            <li className="News-item">
+              <img className="image" src={article.urlToImage} alt="img" />
+              <div className="news-details">
+                <h3>{article.title}</h3>
+                <p>{article.description}</p>
+                <a href={article.url}> Read more</a>
+                <h3>{article.source.name}</h3>
+                <p> Author:{article.author}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default News;
